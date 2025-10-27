00cdd93466e6747d3ab71fb0a0f83d4ee06b5a68
Abdulloh <abdullohyoqubjonov110@gmail.com>
2025-10-28 02:39:54 +0500

    Market loyihasi

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-// Supports weights 100-900
+
 import "@fontsource-variable/montserrat";
 import { Toaster } from "react-hot-toast";
 import { Provider } from "react-redux";
